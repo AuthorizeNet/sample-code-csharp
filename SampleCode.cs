5fb8f9e8c6fea4a138a47f6aae77620cd9ac2e3c
brianmc <bmgtech@gmail.com>
2015-06-03 11:41:52 -0700

    Added Visa Checkout Samples

---
+++ SampleCode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net.authorize.sample
+{
+    class SampleCode
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                ShowUsage();
+                return;
+            }
+
+            switch(args[0])
+            {
+                case "VisaCheckoutDecrypt" :
+                    VisaCheckoutDecrypt.Run();
+                    break;
+                case "VisaCheckoutTransaction":
+                    VisaCheckoutTransaction.Run();
+                    break;
+                default:
+                    ShowUsage();
+                    break;
+            }
+
+            Console.ReadLine();
+           
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usage : SampleCode <CodeSampleName>");
+            Console.WriteLine("");
+            Console.WriteLine("Code Sample Names: ");
+            Console.WriteLine("");
+            Console.WriteLine("         VisaCheckoutDecrypt");
+            Console.WriteLine("         VisaCheckoutTransaction");
+            Console.ReadLine();
+        }
+    }
+}
