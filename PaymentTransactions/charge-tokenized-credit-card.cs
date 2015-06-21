3a41c75a9845409c119d6730d8fbddab358869b6
Calvin Brown <calvin@kairuconsulting.com>
2015-06-21 11:25:13 -0400

    Stubs for classes
    Blank classes added for the implementation of the PaymentTransactions
    and CustomerProfiles

---
+++ PaymentTransactions/charge-tokenized-credit-card.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net.authorize.sample.PaymentTransactions
+{
+    class charge_tokenized_credit_card
+    {
+    }
+}
