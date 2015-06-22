954d577c7473c658fa4a86f5c4a0b2fb26f79342
Calvin Brown <calvin@kairuconsulting.com>
2015-06-22 12:03:52 -0400

    Added the stub for charge-credit-card.cs

---
+++ PaymentTransactions/charge-credit-card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net.authorize.sample
+{
+    public class charge_credit_card
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Charge Credit Card");
+        }
+    }
+}
