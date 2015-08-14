24915249b82a8c0c94406d7b60edebd995b78f71
hpreslier <heatherpreslier@hotmail.com>
2015-08-13 17:59:28 -0700

    Added more sample codes for the customer profile

--- CustomerProfiles/GetCustomerPaymentProfile.cs
+++ CustomerProfiles/GetCustomerPaymentProfile.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AuthorizeNet.Api.Contracts.V1;
+using AuthorizeNet.Api.Controllers;
+using AuthorizeNet.Api.Controllers.Bases;
+
 
 namespace net.authorize.sample
 {
     class GetCustomerPaymentProfile
     {
+        public static void Run(String ApiLoginID, String ApiTransactionKey)
+        {
+            Console.WriteLine("Get Customer Payment Profile sample");
+
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.SANDBOX;
+            // define the merchant information (authentication / transaction id)
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.MerchantAuthentication = new merchantAuthenticationType()
+            {
+                name = ApiLoginID,
+                ItemElementName = ItemChoiceType.transactionKey,
+                Item = ApiTransactionKey,
+            };
+
+            var request = new getCustomerPaymentProfileRequest();
+            request.customerProfileId = "36594444";
+            request.customerPaymentProfileId = "33084787";
+
+            // instantiate the controller that will call the service
+            var controller = new getCustomerPaymentProfileController(request);
+            controller.Execute();
+
+            // get the response from the service (errors contained if any)
+            var response = controller.GetApiResponse();
+
+            if (response.messages.resultCode == messageTypeEnum.Ok)
+            {
+                Console.WriteLine(response.messages.message[0].text);
+                Console.WriteLine("Customer Payment Profile Id: " + response.paymentProfile.customerPaymentProfileId);
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.messages.message[0].code + "  " +
+                                  response.messages.message[0].text);
+            }
+        }
     }
 }
--- CustomerProfiles/GetCustomerProfile.cs
+++ CustomerProfiles/GetCustomerProfile.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AuthorizeNet.Api.Contracts.V1;
+using AuthorizeNet.Api.Controllers;
+using AuthorizeNet.Api.Controllers.Bases;
+
 
 namespace net.authorize.sample
 {
     class GetCustomerProfile
     {
+        public static void Run(String ApiLoginID, String ApiTransactionKey)
+        {
+            Console.WriteLine("Get Customer Profile sample");
+
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.SANDBOX;
+            // define the merchant information (authentication / transaction id)
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.MerchantAuthentication = new merchantAuthenticationType()
+            {
+                name = ApiLoginID,
+                ItemElementName = ItemChoiceType.transactionKey,
+                Item = ApiTransactionKey,
+            };
+
+            var request = new getCustomerProfileRequest();
+            request.customerProfileId = "36594444";
+
+            // instantiate the controller that will call the service
+            var controller = new getCustomerProfileController(request);
+            controller.Execute();
+
+            // get the response from the service (errors contained if any)
+            var response = controller.GetApiResponse();
+
+            if (response.messages.resultCode == messageTypeEnum.Ok)
+            {
+                Console.WriteLine(response.messages.message[0].text);
+                Console.WriteLine("Customer Profile Id: " + response.profile.customerProfileId);
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.messages.message[0].code + "  " +
+                                  response.messages.message[0].text);
+            }
+        }
     }
 }
