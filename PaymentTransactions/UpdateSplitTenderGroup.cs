7804bb7dbbb0def4ea9981fed7d6bf9a7071d435
sapta89 <sbasu2324@gmail.com>
2015-10-08 15:50:58 +0530

    Apple pay create transaction

---
+++ PaymentTransactions/UpdateSplitTenderGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace net.authorize.sample.PaymentTransactions
+{
+    class UpdateSplitTenderGroup
+    {
+        public static void Run(String ApiLoginID, String ApiTransactionKey)
+        {
+            
+        }
+    }
+}
