284ba9ed38e24a15a3657954cbcb45a050425302
Sunny Raj Rathod <sunnyrajrathod@users.noreply.github.com>
2016-11-25 12:13:03 +0530

    Update GetMerchantDetails.cs

--- TransactionReporting/GetMerchantDetails.cs
+++ TransactionReporting/GetMerchantDetails.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace net.authorize.sample.TransactionReporting
+namespace net.authorize.sample
 {
     class GetMerchantDetails
     {
