131c8da55b430c6c05622b5e5fc715713a43639c
Brian McManus <brianmc@users.noreply.github.com>
2016-12-29 07:34:14 -0800

    Update ApproveOrDeclineHeldTransaction.cs

--- FraudManagement/ApproveOrDeclineHeldTransaction.cs
+++ FraudManagement/ApproveOrDeclineHeldTransaction.cs
@@ -8,7 +8,7 @@ using AuthorizeNet.Api.Controllers;
 using AuthorizeNet.Api.Contracts.V1;
 using AuthorizeNet.Api.Controllers.Bases;
 
-namespace net.authorize.sample.PaymentTransactions
+namespace net.authorize.sample.FraudManagement
 {
     class ApproveOrDeclineHeldTransaction
     {
