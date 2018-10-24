9e4adf3c2bd6c572ae7bafa8bb8616ec0bf6e412
ashtru <ashutosh41093@gmail.com>
2018-10-24 09:42:45 +0530

    Removed unused using statements.

--- SampleCode.cs
+++ SampleCode.cs
@@ -1,10 +1,6 @@
 using net.authorize.sample.MobileInappTransactions;
 using net.authorize.sample.PaymentTransactions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using net.authorize.sample.CustomerProfiles;
 
 namespace net.authorize.sample
