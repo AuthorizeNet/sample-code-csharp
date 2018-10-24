3a58293daba9ce3f73394ea8940e8bbe7459d0d3
kapilkumar99 <36911550+kapilkumar99@users.noreply.github.com>
2018-10-24 13:47:36 +0530

    PR1992#Modified files 1)GetAccountUpdaterJobDetails.cs 2)TestRunner.cs

--- SampleCodeTest/TestRunner.cs
+++ SampleCodeTest/TestRunner.cs
@@ -42,51 +42,66 @@ namespace SampleCodeTest
         [Test]
         public void TestAllSampleCodes()
         {
-            string fileName = Constants.CONFIG_FILE;
-            StreamReader reader = File.OpenText(fileName);
-            TestRunner tr = new TestRunner();
-            var numRetries = 3;
 
-            string line;
-            while ((line = reader.ReadLine()) != null)
+            try
             {
-                string[] items = line.Split('\t');
-                
-                string apiName = items[0];
-                string isDependent = items[1];
-                string shouldApiRun = items[2];
-
-                if (!shouldApiRun.Equals("1"))
-                    continue;
-
-                Console.WriteLine(new String('-', 20));
-                Console.WriteLine("Running test case for :: " + apiName);
-                Console.WriteLine(new String('-', 20));
-                ANetApiResponse response = null;
-                for (int i = 0; i < numRetries; ++i)
+                string fileName = Constants.CONFIG_FILE;
+                using (StreamReader reader = File.OpenText(fileName))
                 {
-                    try
+                    TestRunner tr = new TestRunner();
+                    var numRetries = 3;
+
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
                     {
-                        if (isDependent.Equals("0"))
+                        string[] items = line.Split('\t');
+
+                        string apiName = items[0];
+                        string isDependent = items[1];
+                        string shouldApiRun = items[2];
+
+                        if (!shouldApiRun.Equals("1"))
+                            continue;
+
+                        Console.WriteLine(new String('-', 20));
+                        Console.WriteLine("Running test case for :: " + apiName);
+                        Console.WriteLine(new String('-', 20));
+                        ANetApiResponse response = null;
+                        for (int i = 0; i < numRetries; ++i)
                         {
-                            response = InvokeRunMethod(apiName);
-                        }
-                        else
-                        {
-                            response = (ANetApiResponse)typeof(TestRunner).GetMethod("Test" + apiName).Invoke(tr, new Object[] { });
+                            try
+                            {
+                                if (isDependent.Equals("0"))
+                                {
+                                    response = InvokeRunMethod(apiName);
+                                }
+                                else
+                                {
+                                    response = (ANetApiResponse)typeof(TestRunner).GetMethod("Test" + apiName).Invoke(tr, new Object[] { });
+                                }
+
+                                if ((response != null) && (response.messages.resultCode == messageTypeEnum.Ok))
+                                    break;
+
+                            }
+                            catch (Exception ex)
+
+                            {
+                                Console.WriteLine(ex.ToString());
+                            }
                         }
 
-                        if ((response != null) && (response.messages.resultCode == messageTypeEnum.Ok))
-                            break;
+                        Assert.IsNotNull(response);
+                        Assert.AreEqual(response.messages.resultCode, messageTypeEnum.Ok);
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(apiName);
-                        Console.WriteLine(e.ToString());
-                    }
+
                 }
-                Assert.IsNotNull(response);
-                Assert.AreEqual(response.messages.resultCode, messageTypeEnum.Ok);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
             }
         }
 
@@ -435,5 +450,9 @@ namespace SampleCodeTest
         //{
         //    return GetAccountUpdaterJobSummary.Run(apiLoginId, transactionKey);
         //}
+        public ANetApiResponse TestGetAccountUpdaterJobDetails()
+        {
+            return GetAccountUpdaterJobDetails.Run(apiLoginId, transactionKey);
+        }
     }
 }
--- TransactionReporting/GetAccountUpdaterJobDetails.cs
+++ TransactionReporting/GetAccountUpdaterJobDetails.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using AuthorizeNet.Api.Contracts.V1;
 using AuthorizeNet.Api.Controllers;
 using AuthorizeNet.Api.Controllers.Bases;
+using System.Collections;
 
 namespace net.authorize.sample
 {
@@ -25,20 +26,20 @@ namespace net.authorize.sample
             };
 
             // parameters for request
-            string month = "2017-06";
-            string modifiedTypeFilter = "all"
+            string month = "2018-08"; //"2017-06";
+            string modifiedTypeFilter = "all";
 
-            var request = new getAccountUpdaterJobDetailsRequest();
+            var request = new getAUJobDetailsRequest();
             request.month = month;
-            request.modifiedTypeFilter = modifiedTypeFilter;
+            request.modifiedTypeFilter = AUJobTypeEnum.all;
             request.paging = new Paging
             {
-                limit = 1000,
+                limit = 100,
                 offset = 1
             };
 
             // instantiate the controller that will call the service
-            var controller = new getAccountUpdaterJobDetailsController(request);
+            var controller = new getAUJobDetailsController(request);
             controller.Execute();
 
             // get the response from the service (errors contained if any)
@@ -46,31 +47,81 @@ namespace net.authorize.sample
 
             if (response != null && response.messages.resultCode == messageTypeEnum.Ok)
             {
+                Console.WriteLine("SUCCESS: Get Account Updater job details for Month and year : " + month);
                 if (response.auDetails == null)
+                {
+                    Console.WriteLine("No GetAccountUpdaterjobdetails for this month and year.");
                     return response;
-
-                foreach (var update in response.auDetails.auUpdate)
-                {
-                    Console.WriteLine("Profile ID / Payment Profile ID: {0} / {1}", update.customerProfileID, update.customerPaymentProfileID);
-                    Console.WriteLine("Update Time (UTC): {0}", update.updateTimeUTC);
-                    Console.WriteLine("Reason Code: {0}", update.auReasonCode);
-                    Console.WriteLine("Reason Description: {0}", update.reasonDescription);
                 }
 
-                foreach (var delete in response.auDetails.auDelete)
+                // Displaying the Audetails of each response in the list
+                foreach (var details in response.auDetails)
                 {
-                    Console.WriteLine("Profile ID / Payment Profile ID: {0} / {1}", delete.customerProfileID, update.customerPaymentProfileID);
-                    Console.WriteLine("Update Time (UTC): {0}", delete.updateTimeUTC);
-                    Console.WriteLine("Reason Code: {0}", delete.auReasonCode);
-                    Console.WriteLine("Reason Description: {0}", delete.reasonDescription);
+
+
+                    Console.WriteLine(" **** Customer profile details Start ****");
+                    Console.WriteLine("Profile ID / Payment Profile ID: {0} / {1}", details.customerProfileID, details.customerPaymentProfileID);
+                    Console.WriteLine("Firstname lastname : {0} / {1}", details.firstName, details.lastName);
+                    Console.WriteLine("Update Time (UTC): {0}", details.updateTimeUTC);
+                    Console.WriteLine("Reason Code: {0}", details.auReasonCode);
+                    Console.WriteLine("Reason Description: {0}", details.reasonDescription);
+
+
+                    if (details is auUpdateType)
+                    {
+                        for (int i = 0; i < ((auUpdateType)details).subscriptionIdList.Length; i++)
+                        {
+                            Console.WriteLine("SubscriptionIdList: {0}", ((auUpdateType)details).subscriptionIdList[i]);
+                        }
+                    }
+                    else if (details is auDeleteType)
+                    {
+                        for (int i = 0; i < ((auDeleteType)details).subscriptionIdList.Length; i++)
+                        {
+                            Console.WriteLine("SubscriptionIdList: {0}", ((auDeleteType)details).subscriptionIdList[i]);
+                        }
+                    }
+
+
+                    if (details.GetType().GetField("newCreditCard") != null)
+                    {
+                        Console.WriteLine("Fetching New Card Details");
+                        // Fetching New Card Details
+                        var newCreditCard = details.GetType().GetField("newCreditCard").GetValue(details);
+                        creditCardMaskedType newCreditCardMaskedType = (creditCardMaskedType)newCreditCard;
+                        Console.WriteLine("Card Number: {0}", newCreditCardMaskedType.cardNumber);
+                        Console.WriteLine("New Expiration Date: {0}", newCreditCardMaskedType.expirationDate);
+                        Console.WriteLine("New Card Type: {0}", newCreditCardMaskedType.cardType);
+
+                    }
+
+                    if (details.GetType().GetField("oldCreditCard") != null)
+                    {
+                        Console.WriteLine("Fetching Old Card Details");
+                        // Fetching Old Card Details
+                        var oldCreditCard = details.GetType().GetField("oldCreditCard").GetValue(details);
+                        creditCardMaskedType oldCreditCardMaskedType = (creditCardMaskedType)oldCreditCard;
+                        Console.WriteLine("Old Card Number: {0}", oldCreditCardMaskedType.cardNumber);
+                        Console.WriteLine("Old Expiration Date: {0}", oldCreditCardMaskedType.expirationDate);
+                        Console.WriteLine("Old Card Type: {0}", oldCreditCardMaskedType.cardType);
+
+                        Console.WriteLine("**** Customer profile details End ****");
+                    }
                 }
+
             }
+
             else if (response != null)
             {
                 Console.WriteLine("Error: " + response.messages.message[0].code + "  " +
-                                    response.messages.message[0].text);
+                                  response.messages.message[0].text);
             }
-
+            else if (response == null)
+            {
+                var errResponse = controller.GetErrorResponse();
+                Console.WriteLine("Error: " + errResponse.messages.message[0].code + "  " +
+                                  response.messages.message[0].text);
+            }
             return response;
         }
     }
